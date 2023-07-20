7aa7d3efd7cf28aa52cb9f0c9504753207d11f7d
clhilgert <claytonhilgertmst@gmail.com>
2023-07-19 22:21:36 -0500

    updated

---
+++ react-chrome-ext/src/components/Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Day from './Day.tsx';
+
+export const Main = () => {
+  return (
+    <div className='main-container'>
+      <Day/>
+      <Day/>
+      <Day/>
+      <Day/>
+      <Day/>
+      <Day/>
+      <Day/>
+    </div>
+  )
+}
